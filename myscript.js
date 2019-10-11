738d57b06130a8b2beadce6109c6e4888454ebaf
Zeeshan  Manzoor <zeeshanmanzoor.zm@gmail.com>
2019-10-12 01:40:36 +0500

    asg 2

---
+++ myscript.js
@@ -0,0 +1,160 @@
+//  var yourcity = prompt("enter your city");
+//  if(yourcity==="karachi"){
+//       alert("welcome to city of light");
+//  }
+//  else{
+//      alert(" wrong city")
+//  }
+
+//  var gender = prompt("enter your gender");
+//  if(gender==="male" || gender==="Male"){
+//     alert("welcome sir")
+//  }
+//  else if(gender==="female"|| gender==="Female"){
+
+//  }
+//  else{
+//      alert("check your gender")
+//  }
+
+
+// var color = prompt("enter color plz");
+// if(color==="red"){
+//     alert("stop plz")
+// }
+// else if(color==="yellow"){
+//     alert("ready to move")
+// }
+// else if(color==="green"){
+//     alert("lets go")
+// }
+    // else{
+    //     alert("wrong color")
+    // }
+
+    //  var fuel = +prompt("enter your fuel")
+    //  if(fuel<0.25){
+    //      alert("fill your tank")
+
+    //  }
+    //  else{
+    //      alert("much fuel")
+    //  }
+
+    // var a = 4 ;
+    //  if(++a === 5){
+    //      alert("given condition is true")
+
+    //  }
+
+    //  var c = 12;
+    //  if(c++ ==13){
+    //      alert("c")
+    //  }
+
+    //  var c = 12; 
+    //  if (c++ === 13){ 
+    //      alert("condition 1 is true" + c );
+    //      } 
+    //  if (c === 13)
+    //  { alert("condition 2 is true"); } 
+    //  if (++c < 14){ alert("condition 3 is true"); }
+    //   if(c === 14){ alert("condition 4 is true"); }  
+
+    // var materialCost = 20000;
+    //  var laborCost = 2000; 
+    //  var totalCost = materialCost + laborCost;
+    //   if (totalCost === laborCost + materialCost)
+    //   { alert("The cost equals"); }  
+
+    //   if (true){ alert("True"); } if (false){ alert("False"); }  
+
+
+    //   if("car" < "cat"){ alert("car is smaller than cat"); }
+
+
+    //  var marksobtained = prompt("marks obtained");
+    //  var totalmarks = 500;
+    //  var percentage =  (marksobtained / totalmarks)* 100
+    //  alert(percentage + "%" )
+    //  if(percentage >= 80 ){
+    //      var remarks = "A grade";
+    //  } 
+    //    else  if(percentage >= 70   ){
+    //     var remarks = "B grade";
+    //  } 
+    //  else if(percentage >=60  ){
+    //     var remarks = "C grade";
+    //  }
+    //  else if(percentage >=50  ){
+    //     var remarks = "D grade";
+    //  }
+    //  else if(percentage < 40  ){
+    //     var remarks = "fail";
+    //  }
+    // document.write("marks obtained : " + marksobtained +  "<br/>");
+    // document.write("total marks : " + totalmarks +  "<br/>");
+    // document.write("total percentage : " + percentage +  "<br/>");
+    // document.write("remarks  " +  remarks)
+
+
+    //  var a = +prompt("enter any number");
+    //  if(a %3 ==0){
+    //   alert("num is divisible")
+    //  }else(
+    //      alert("not")
+    //  )
+
+    // var num = +prompt("enter any number")
+    // if(num%2==0){
+    //     alert("even num")
+    // }
+    // else{
+    //     alert("odd")
+    // }
+
+
+    //  var a = 5;
+    //  var guessNumber = +prompt("guess the number");
+
+    //  if(guessNumber === a){
+    //     alert("bingo");
+    //  }else{
+    //     alert ("try again")
+    //  }
+
+
+//     var temp = +prompt("enter temperature");
+//     if(temp >= 40){
+//      alert("too hot to handle")
+//     }
+//     else if(temp > 30){
+//         alert("today is normal")
+//     }
+//    else if(temp > 20){
+//        alert("weather is cool")
+
+//     }
+//     else if(temp > 10){
+//         alert("weather is cool and freezy")
+//     }
+
+//  var num = +prompt("enter num one");
+//  var secNum = +prompt("enter num two");
+//  var opr = prompt("enter any operator");
+//  if(opr === "+" ){
+//       var add = num + secNum
+//     alert(  add  )
+//  }
+//  else if(opr === "-" ){
+//    var min = num - secNum;
+//     alert( min)
+//  }
+//  else if(opr === "*" ){
+//    var mul = num * secNum
+//     alert(mul )
+//  }
+//  else if(opr === "/" ){
+//     var div = num / secNum
+//      alert(div )
+//   }
---
+++ myscript2.js
@@ -0,0 +1,103 @@
+
+
+// var a = +prompt("enter number one");
+// var b = +prompt("enter number two");
+// if(a>b){
+//     alert("a is greator")
+// }
+// else if(a===b){
+//     alert("a is equal to b")
+// }
+// else if(b>a){
+//     alert("b is  greator thena")
+// }
+// else{
+//     alert(" try again")
+// }
+
+
+// var num = +prompt("enter any number ");
+// if(num < 0){
+//     alert("neg num")
+// }
+// else if(num >0 ){
+//     alert("positive num")
+// }
+// else if (num === 0){
+//     alert("num is zero")
+// }
+// else{
+//     alert(" try agian")
+// }
+
+//   var char = prompt("enter string");
+//   if(char==='a'||char==='e'||char==='i'||char==='o'|| char==='u'){
+// alert("the char is vowel")
+//   }
+//  else{
+//      alert("this is not vowels")
+//  }
+
+
+
+
+
+// var greeting; 
+// var hour = 13; 
+// if (hour < 18) { 
+//     greeting = "Good day"
+// } 
+// else{
+//     greeting = "Good evening";
+//  }  
+
+//  document.write("hello boss have a   " + greeting)
+
+
+
+//  var time =+prompt("enter time plz")
+//  if(time>=0000 && time <=1200){
+//            alert("good morning dear")
+//  }
+//  else if(time>=1200 && time <=1700){
+//     alert("good afternoon dear")
+// }
+// else if(time>=1700 && time <=2100){
+//     alert("good morning dear")
+// }
+// else if(time>=2100 && time <=2400){
+//     alert("good morning dear")
+// }
+// else{
+//     alert("plz enter correct time")
+// }
+
+
+ 
+
+//  var ch = prompt("enter any number")
+// if (ch>=65 && ch<=90){
+//     alert("Character is a capital letter")
+// }
+// else if (ch>=97 && ch<=122){
+//     alert("Character is a small letter")
+// }
+
+// else if (ch>=48 && ch<=57){
+//     alert("Character is a digit")
+// }
+
+// else if ((ch>0 && ch<=47)||(ch>=58 && ch<=64)||
+//        (ch>=91 && ch<=96)||(ch>=123 && ch<=127)){
+//  alert("char is symbol")
+//        }
+
+
+//  var login = "hello world";
+// var passwrd = prompt("enter paswrd");
+
+// if(passwrd===login){
+//    alert("wellcome sir")
+// }else{
+//   alert("try again")
+// }
